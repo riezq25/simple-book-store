dfc1efbbb2d16408170eddc9c227ffbf479432f0
Rizqi Ari Putra <riezq.25@gmail.com>
2024-10-31 09:04:01 +0700

    [FEAT] Database seeder

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    protected $model = \App\Models\Book::class;
+
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'author' => $this->faker->name,
+            'year' => $this->faker->year,
+            'price' => $this->faker->numerify('##000'),
+        ];
+    }
+}
---
+++ database/seeders/BookSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Book;
+
+class BookSeeder extends Seeder
+{
+    public function run()
+    {
+        // Menggunakan factory untuk membuat 50 data buku
+        Book::factory()
+            ->count(100)
+            ->create();
+    }
+}
